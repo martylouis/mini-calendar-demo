686d8460d822db41a802c062fcdb958cfa6a77b0
Marty Thierry <marty@martylouis.com>
2020-12-09 01:45:05 -0600

    Update mini cal events
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- components/mini-calendar.js
+++ components/mini-calendar.js
@@ -32,7 +32,12 @@ const MiniCalendar = () => {
       </div>
       <div className="ba-mini-calendar__event_list">
         <div className="ba-mini-calendar__event_item block block_event">AH Thursday Meeting 1WS</div>
-        <div className="ba-mini-calendar__event_item block block_event">AH Friday Meeting 2WS</div>
+        <div className="ba-mini-calendar__event_item block block_event">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+            <path id="Path_22" data-name="Path 22" d="M7.2,10.4H8.8V12H7.2ZM7.2,4H8.8V8.8H7.2Zm.792-4a8,8,0,1,0,5.662,2.34A8,8,0,0,0,7.992,0ZM8,14.4A6.4,6.4,0,1,1,14.4,8,6.4,6.4,0,0,1,8,14.4Z" fill="#f46e00" />
+          </svg>
+          AH Friday Meeting 2WS
+        </div>
         <div className="ba-mini-calendar__event_item block block_task">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
             <path id="Path_1111" data-name="Path 1111" d="M8,0a8,8,0,1,0,8,8A8.024,8.024,0,0,0,8,0ZM8,12V9H4V7H8V4l4,4Z" />
